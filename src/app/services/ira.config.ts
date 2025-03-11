d62de7e2a702690f4350ee135af2c8ff76181d4e
venlancer <venlancer@gmail.com>
2025-03-11 13:10:25 +0530

    changes

---
+++ src/app/services/ira.config.ts
@@ -0,0 +1,6 @@
+export const AWS_CONFIG = {
+    accessKeyId: 'AKIA3ISBVTFUUJOBZ3GM',
+    secretAccessKey: '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',
+    region: 'eu-north-1', // Change region as needed,
+    bucketName: 'abstract-submissions'
+}
